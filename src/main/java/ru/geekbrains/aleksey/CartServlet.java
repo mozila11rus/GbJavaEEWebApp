2cc1824cb630d96c54399edff6e251e0ce8f393d
Aleksey Chizhov <8mozila8@rambler.ru>
2020-03-23 22:06:15 +0300

    HomeWork_2

---
+++ src/main/java/ru/geekbrains/aleksey/CartServlet.java
@@ -0,0 +1,44 @@
+package ru.geekbrains.aleksey;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+import java.io.Serializable;
+
+@WebServlet(name = "CartServlet", urlPatterns = "/cart")
+public class CartServlet implements Servlet, Serializable {
+    private Logger logger = LoggerFactory.getLogger(MainServlet.class);
+    private transient ServletConfig config;
+
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+        this.config = config;
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return this.config;
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        logger.info("New request");
+
+        servletResponse.getWriter().println("<h1>Корзина</h1>");
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Hello from CartServlet";
+    }
+
+    @Override
+    public void destroy() {
+        logger.info("CartServlet class destroy");
+    }
+
+//    resp.setHeader("Content-type","text/html; charset=utf8");
+}
---
+++ src/main/java/ru/geekbrains/aleksey/MainServlet.java
@@ -0,0 +1,21 @@
+package ru.geekbrains.aleksey;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+public class MainServlet extends HttpServlet {
+    private Logger logger = LoggerFactory.getLogger(MainServlet.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("<h1>Главная страница</h1>");
+    }
+}
